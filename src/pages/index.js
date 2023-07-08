c9af6ae8aebcdd1ffc3a2bb578a88c3f6e37c430
“seeinplays” <seeinplays@gmail.com>
2023-07-08 16:24:43 +0200

    wallet connect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.css';
-import SwipeCards from '@/pages/SwipeCards';
-import { Web3Button } from '@web3modal/react'
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import Web3Button from '@/components/Web3Button'
 
 const inter = Inter({ subsets: ['latin'] });
 
