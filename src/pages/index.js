090d84e4365b856b92c2462cfbf3575152bca356
jennyg0 <jen2348@hotmail.com>
2023-07-08 23:41:12 +0200

    fix fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head';
-import { Inter } from 'next/font/google';
+
 import styles from '@/styles/Home.module.css';
 import SwipeCards from '@/pages/SwipeCards';
+import { Montserrat } from '@next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const montserrat = Montserrat({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -14,7 +15,7 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main className={`${styles.main} ${montserrat.className}`}>
         Gather
         <SwipeCards />
       </main>
