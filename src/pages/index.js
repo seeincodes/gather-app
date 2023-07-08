dccd61e9a7cc3b0dc70bc684d4993e7d99cb9614
“seeinplays” <seeinplays@gmail.com>
2023-07-08 21:47:56 +0200

    fix error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { Web3Button } from '@web3modal/react';
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Web3Button from '@/components/Web3Button'
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <>
-    <Web3Button />
+      <Web3Button />
     </>
   );
 }
