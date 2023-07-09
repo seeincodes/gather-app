663ee3e5b7da7ad3c2d2d4c82dcef48cc7eedbb7
Xian Zheng <seeinplay@gmail.com>
2023-07-09 09:45:57 +0200

    scroll

--- contracts/token/hardhat.config.js
+++ contracts/token/hardhat.config.js
@@ -2,12 +2,17 @@ require('@nomicfoundation/hardhat-toolbox');
 
 module.exports = {
   solidity: '0.8.9',
-  defaultNetwork: 'gnosis',
+  defaultNetwork: 'scroll',
   networks: {
     gnosis: {
       url: 'https://rpc.gnosischain.com/',
       gasPrice: 2000000000,
       accounts: [''],
+    },
+    scroll: {
+      url: 'https://scroll-alphanet.public.blastapi.io' || '',
+      accounts:
+        [''],
     },
   },
 };
